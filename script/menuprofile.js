eda3004ff3bfaae91da6b9e0d3997a77c2e61555
Thani Paknam <64030074@kmitl.ac.th>
2024-03-24 18:37:14 +0700

    update profile

---
+++ script/menuprofile.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuContent = document.getElementById("menuContent");
+    if (menuContent.style.display === "none" || menuContent.style.display === "") {
+        menuContent.style.display = "block";
+    } else {
+        menuContent.style.display = "none";
+    }
+}
